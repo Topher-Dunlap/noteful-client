11d84342048e06d5d0cc1872bf9a9071b1db7854
topher-dunlap <thelonioustopher@yahoo.com>
2021-01-20 22:52:30 -0600

    removing console.log

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -15,7 +15,6 @@ export default class Note extends React.Component {
   handleClickDelete = e => {
     e.preventDefault()
     const noteId = this.props.id
-      console.log("handleClickDelete inside note.js", noteId)
 
     fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
       method: 'DELETE',
